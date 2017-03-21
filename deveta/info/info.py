ef79bec57bde36942eb3d0603c45be78d38c9c57
James Stronz <james@network-perception.com>
2017-03-20 21:29:11 -0500

    add support for more than one argument in show

--- deveta/info/info.py
+++ deveta/info/info.py
@@ -3,8 +3,12 @@ from __future__ import print_function
 
 __all__ = ['show']
 
-def show(target):
-    print(type(target), target)
+def show(*targets):
+    _targets = [ ]
+    for target in targets:
+        _targets.append(type(target)) 
+        _targets.append(target)
+    print(*_targets)
 
 if __name__ == "__main__":
     pass
