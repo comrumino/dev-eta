518d0e409d5bd17d115fd1b32f9b29a1d7142b1d
James Stronz <james@network-perception.com>
2017-03-20 21:10:31 -0500

    added debugging info

--- deveta/__init__.py
+++ deveta/__init__.py
@@ -1,8 +1,12 @@
-"""a python library for the miscellaneous."""
+""" a python library for the miscellaneous. """
 __author__ = 'James Stronz'
 __author_email__ = 'j.a.stronz@gmail.com'
 __license__ = 'GPLv3'
 __url__ = 'https://github.com/strayArch/dev-eta'
 __version__ = '1.0.0'
 
+__all__ = ['info', 'locate']
+
+
+import info
 import locate
---
+++ deveta/info/info.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python2
+from __future__ import print_function
+
+__all__ = ['show']
+
+def show(target):
+    print(type(target), target)
+
+if __name__ == "__main__":
+    pass
