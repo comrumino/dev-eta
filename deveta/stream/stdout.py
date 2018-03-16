386677c020511f49ed4fc63b36e0603bc5bc70df
James Stronz <james@network-perception.com>
2018-03-16 02:33:10 -0500

    deveta/stream/stdout.py: grammar and orthography changes

--- deveta/stream/stdout.py
+++ deveta/stream/stdout.py
@@ -1,17 +1,17 @@
 """This module is for methods that print to the stdout stream. Currently,
 the main purpose is to print text with a non-default foreground color. The colors supported
-are the eight basic ASNI terminal colors: 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', and 'white'.
+are the eight basic ANSI terminal colors: 'black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', and 'white'.
 """
 from __future__ import print_function
 from ansi_colors import FG_COLORS
 __all__ = ['kprint', 'rprint', 'gprint', 'yprint', 'bprint', 'mprint', 'cprint', 'wprint']
 
 
-"""Explaination of ANSI_ESC_SEQ syntac:
+"""Explanation of `_ANSI_ESC_SEQ` syntax:
     \\x := the following two characters are hex-digits
     1b hex-digits := the escape control character
     [ := Control Sequence Introducer
-Extra resources:
+Additional resources:
     https://en.wikipedia.org/wiki/ANSI_escape_code
     http://wiki.bash-hackers.org/scripting/terminalcodes
 """
@@ -19,23 +19,23 @@ _ANSI_ESC_SEQ = "\x1b[{}m"
 
 
 class InvalidForegroundColorError(Exception):
-    """Raised within `_format_fg_color` when invalid color specified"""
+    """Raised within `_format_fg_color` when a color other than one of the eight basic ANSI terminal colors is specified."""
     pass
 
 
 def _format_fg_color(target, color):
     """Purpose:
-        Format `target` object using ansi `color` from `ansi_colors.FG_COLORS`.
+        Format `target` object using ANSI `color` from `ansi_colors.FG_COLORS`.
     Parameters:
         `target`:
-            instance type: any object such that the __str__ method is defined
+            Instance type: any object such that the __str__ method is defined
         `color`:
-            instance type: any object such that the __hash__ method is defined
-            values: FG_COLORS.keys()
+            Instance type: any object such that the __hash__ method is defined
+            Values: FG_COLORS.keys()
     Raise:
         `InvalidForegroundColorError`:
-            if color is not in FG_COLORS
-        *plus built-in exceptions if they occur
+            If color is not in FG_COLORS
+        *Plus built-in exceptions if they occur
     Return:
         String object
     """
@@ -49,38 +49,37 @@ def _format_fg_color(target, color):
 
 def _color_args(*args, **kwargs):
     """Purpose:
-        reformat arguments that will be printed so that they are the
-        desired ANSI color.
+        Reformat arguments so that they will be printed using the desired ANSI color.
     Parameters:
         `args`:
-            instance type: any object such that the __str__ method is defined
+            Instance type: any object such that the __str__ method is defined
         `kwargs`:
             `fg_color`:
-                instance type: string
-                values: FG_COLORS.keys()
+                Instance type: string
+                Values: FG_COLORS.keys()
     Raise:
         `InvalidForegroundColorError`:
-            This propagates from `_color_args`.
-        *plus built-in exceptions if they occur
+            This exception propagates from `_color_args`.
+        *Plus built-in exceptions if they occur
     Returns:
-        list of strings which are wrapped with ANSI escapes
+        list of ANSI wrapped strings
     """
     return [_format_fg_color(arg, kwargs['fg_color']) for arg in args]
 
 
 def _ansi_print(*args, **kwargs):
     """Purpose:
-        Print ansi foreground colored text using `future_print`. It accepts the same arguments
-        and keyword-arguments as `future_print`, plus the additional keyword-argument `fg_color`.
+        Print ANSI foreground colored text using `future_print`. It accepts the same arguments
+        and keyword-arguments as `future_print`, plus the additional keyword-argument of `fg_color`.
     Parameters:
-        Extra parameters besides those defined for `future_print`.
+        Extra parameters besides those defined for `future_print`...
         `fg_color`:
-            instance type: string
-            values: FG_COLORS.keys()
+            Instance type: string
+            Values: FG_COLORS.keys()
     Raise:
         `InvalidForegroundColorError`:
             This propagates from `_color_args`.
-        *plus built-in exceptions if they occur
+        *Plus built-in exceptions if they occur
     """
     fg_color = kwargs.pop('fg_color')
     args = _color_args(*args, fg_color=fg_color)
@@ -89,7 +88,7 @@ def _ansi_print(*args, **kwargs):
 
 def kprint(*args, **kwargs):
     """Purpose:
-        The first letter of the method name maps to a color below.
+        The first letter of each method-name (`kprint`, `gprint`, ..., and `kprint`) maps to a color below.
             r -> Red
             g -> Green
             y -> Yellow
@@ -102,53 +101,53 @@ def kprint(*args, **kwargs):
         considering that black is a common default, where blue is mapped to b in anticipation of the
         user's disposition while using this module --- printing text with a non-default foreground color.
     Parameters:
-        Accepts the same arguments and keyword-arguments as future_print
+        Accepts the same arguments and keyword-arguments as `future_print`
     Raise:
         `InvalidForegroundColorError`:
-            This propagates from `_color_args`.
-        *plus built-in exceptions if they occur
+            This exception propagates from `_color_args`.
+        *Plus built-in exceptions if they occur
     """
     kwargs.update({'fg_color': 'black'})
     _ansi_print(*args, **kwargs)
 
 
 def rprint(*args, **kwargs):
-    """see docstring under kprint"""
+    """Refer to the docstring for kprint."""
     kwargs.update({'fg_color': 'red'})
     _ansi_print(*args, **kwargs)
 
 
 def gprint(*args, **kwargs):
-    """see docstring under kprint"""
+    """Refer to the docstring for kprint."""
     kwargs.update({'fg_color': 'green'})
     _ansi_print(*args, **kwargs)
 
 
 def yprint(*args, **kwargs):
-    """see docstring under kprint"""
+    """Refer to the docstring for kprint."""
     kwargs.update({'fg_color': 'yellow'})
     _ansi_print(*args, **kwargs)
 
 
 def bprint(*args, **kwargs):
-    """see docstring under kprint"""
+    """Refer to the docstring for kprint."""
     kwargs.update({'fg_color': 'blue'})
     _ansi_print(*args, **kwargs)
 
 
 def mprint(*args, **kwargs):
-    """see docstring under kprint"""
+    """Refer to the docstring for kprint."""
     kwargs.update({'fg_color': 'magenta'})
     _ansi_print(*args, **kwargs)
 
 
 def cprint(*args, **kwargs):
-    """see docstring under kprint"""
+    """Refer to the docstring for kprint."""
     kwargs.update({'fg_color': 'cyan'})
     _ansi_print(*args, **kwargs)
 
 
 def wprint(*args, **kwargs):
-    """see docstring under kprint"""
+    """Refer to the docstring for kprint."""
     kwargs.update({'fg_color': 'white'})
     _ansi_print(*args, **kwargs)
