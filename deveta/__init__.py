6bd24096193be3715f831e1948881cd007e8097d
James Stronz <james@network-perception.com>
2019-12-19 14:16:25 -0600

    Bumped major version, dropped py2 and ported to py3

--- deveta/__init__.py
+++ deveta/__init__.py
@@ -3,7 +3,7 @@ __author__ = 'James Stronz'
 __author_email__ = 'comrumino@archstrike.org'
 __license__ = 'GPLv3'
 __url__ = 'https://github.com/comrumino/deveta'
-__version__ = '1.0.2'
+__version__ = '2.0.0'
 
 __all__ = ['locate', 'stream']
 
